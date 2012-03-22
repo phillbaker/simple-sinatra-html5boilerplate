1d8840e8fa00f1f6707184b32d361a5438167d20
pbaker <me@retrodict.com>
2012-03-21 20:47:01 -0400

    Problem was overriding wrong methods in child template

--- app/views/index.html.rb
+++ app/views/index.html.rb
@@ -1,10 +1,10 @@
 require 'boilerplate.html'
 
 class Index < Erector::Widgets::Html5boilerplate
-  def title
+  def page_title
     @title
   end
-  def content
+  def main_content
     #just add content
     p do
       text @content
